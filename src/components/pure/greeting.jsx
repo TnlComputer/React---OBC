612c2d81c3cf1e95c4cac3e5a4874bd78f4e1555
Jorge Martinez <tnlcomputer@gmail.com>
2022-12-24 10:54:46 -0300

    clono y sigo el proceso del TODO del OB

--- src/components/pure/greeting.jsx
+++ src/components/pure/greeting.jsx
@@ -1,34 +1,50 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 
 class Greeting extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      age: 57,
-    };
-  }
-  render() {
-    return (
-      <div>
-        <h1>Hola {this.props.name} !</h1>
-        <h2>Tu edad es de: {this.state.age}</h2>
-        <div>
-          <button onClick={this.birthday}>Cumplir Años</button>
-        </div>
-      </div>
-    );
-  }
 
-  birthday = () => {
-    this.setState((prevState) => ({
-      age: prevState.age + 1,
-    }));
-  };
+
+    constructor(props){
+        super(props);
+        this.state = {
+           age : 29
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>
+                    ¡HOLA { this.props.name } !
+                </h1>
+                <h2>
+                    Tu edad es de: { this.state.age }
+                </h2>
+                <div>
+                    <button onClick={this.birthday}>
+                        Cumplir años
+                    </button>
+                </div>
+            </div>
+        );
+    }
+
+    birthday = () => {
+        this.setState((prevState) => (
+            {
+                age: prevState.age +1
+            }
+        ))
+    }
+
+
 }
 
+
 Greeting.propTypes = {
-  name: PropTypes.string,
+    name: PropTypes.string,
 };
+
 
 export default Greeting;
--- src/components/pure/greetingF.jsx
+++ src/components/pure/greetingF.jsx
@@ -1,26 +1,39 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
 
 const Greetingf = (props) => {
-  //  Breve Introdiccion a useState
-  const [age, setage] = useState(57);
 
-  const birthday = () => {
-    setage(age + 1);
-  };
-  return (
-    <div>
-      <h1>Hola {props.name} desde componente funcional !</h1>
-      <h2>Tu edad es de: {age}</h2>
-      <div>
-        <button onClick={birthday}>Cumplir Años</button>
-      </div>
-    </div>
-  );
+    // Breve introducción a useState
+    // const [variable, método para actualizarlo] = useState(valor inicial)
+    const [age, setage] = useState(29);
+
+    const birthday = () => {
+        // actualizamos el State
+        setage(age + 1);
+    }
+
+    return (
+        <div>
+            <h1>
+                ¡HOLA { props.name } desde componente funcional !
+            </h1>
+            <h2>
+                Tu edad es de: { age }
+            </h2>
+            <div>
+                <button onClick={birthday}>
+                    Cumplir años
+                </button>
+            </div>
+        </div>
+    );
 };
+
 
 Greetingf.propTypes = {
-  name: PropTypes.string,
+    name: PropTypes.string
 };
+
 
 export default Greetingf;
