ec17d334f934017274ca179bbf451a0723c4411c
Jorge Martinez <tnlcomputer@gmail.com>
2022-12-27 11:19:47 -0300

    agregamos un margen al boton de agregar tarea

--- src/index.js
+++ src/index.js
@@ -8,15 +8,13 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
 
 import App from "./App";
-// import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from "./reportWebVitals";
 // import AppRoutingOne from './AppRoutingOne';
 // import AppRoutingFinal from './AppRoutingFinal';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    {/* <AppRoutingOne /> */}
-    {/* <AppRoutingFinal /> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
